95010ed1f8f923e82aafd0211ad255c99007489b
Durun <negiigainuki@gmail.com>
2020-02-11 20:38:27 +0900

    add command

---
+++ src/main/kotlin/veryFit2googleFit/app/AppCommand.kt
@@ -0,0 +1,9 @@
+package veryFit2googleFit.app
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+object AppCommand: CliktCommand() {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}

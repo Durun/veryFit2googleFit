1a3dbec14d06e0f3d74862d55d1e79777298dbe8
Durun <negiigainuki@gmail.com>
2020-02-14 08:32:49 +0900

    add sleep process

--- src/main/kotlin/io/github/durun/vFit2gFit/app/AppCommand.kt
+++ src/main/kotlin/io/github/durun/vFit2gFit/app/AppCommand.kt
@@ -4,6 +4,7 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.path
 import io.github.durun.vFit2gFit.model.HeartRate
+import io.github.durun.vFit2gFit.model.Sleep
 import io.github.durun.vFit2gFit.vfit.*
 import io.github.durun.vFit2gFit.writer.db.DBWriter
 import kotlinx.serialization.ImplicitReflectionSerializer
@@ -33,24 +34,39 @@ object AppCommand : CliktCommand() {
 		val writer = DBWriter.of(dbPath)
 		(0..100).map { (LocalDateTime.now() - it.days.toJavaDuration()).toLocalDate() }
 				.forEach {
-					writer.recordOn(it)
+					writer.recordHeartRates(it)
+					writer.recordSleeps(it)
 				}
 		println("Total items: $itemCount")
 	}
 
 	@ExperimentalTime
 	@ImplicitReflectionSerializer
-	private fun DBWriter.recordOn(onDate: LocalDate) {
+	private fun DBWriter.recordHeartRates(onDate: LocalDate) {
 		val logs = VFitLogProviders.fromFile(veryfitRoot)
 				.fetchGroups<HealthHeartRateLog, HealthHeartRateItemLog>(onDate)
 				.lastOrNull()
 		val heartRates = logs
 				?.let { (rate, item) ->
 					itemCount += item.size
-					println("$onDate items: ${item.size}")
+					println("$onDate HeartRate items: ${item.size}")
 					HeartRate.of(rate, item)
 				}
 				?: emptyList()
 		this.write(heartRates)
+	}
+
+	@ExperimentalTime
+	@ImplicitReflectionSerializer
+	private fun DBWriter.recordSleeps(onDate: LocalDate) {
+		val logs = VFitLogProviders.fromFile(veryfitRoot)
+				.fetchGroups<HealthSleepLog, HealthSleepItemLog>(onDate)
+				.lastOrNull()
+		val sleeps = logs
+				?.let { (sleep, items) ->
+					Sleep.of(sleep, items)
+				}
+				?.let { println(it) }
+
 	}
 }
